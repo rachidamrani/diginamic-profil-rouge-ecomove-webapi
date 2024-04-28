b21cfef2bf84d8865af26726daf5f01701cea79f
devbymoz <mzaoui.2409@gmail.com>
2024-04-28 18:46:13 +0200

    Connexion d'un utilisateur

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,43 @@
+﻿using ecomove_back.Data.Models;
+using ecomove_back.DTOs.AppUserDTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ecomove_back.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private UserManager<AppUser> _userManager;
+        private SignInManager<AppUser> _signInManager;
+        private RoleManager<IdentityRole> _roleManager;
+
+        public AuthController(
+            UserManager<AppUser> userManager,
+            SignInManager<AppUser> signInManager,
+            RoleManager<IdentityRole> roleManager
+        )
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _roleManager = roleManager;
+        }
+
+
+        [HttpGet("{email}, {password}")]
+        public async Task<IActionResult> SignIn()
+        {
+            var user = await _userManager.get
+
+            _signInManager.SignInAsync().
+
+            return Ok();
+        }
+
+
+
+
+    }
+}
